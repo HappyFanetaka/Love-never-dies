ff975dae54b32c5feb509663ee39e71433b4dc53
HappyFanetaka <23kmf06@gmail.com>
2024-08-20 17:20:41 +0900

    Update kimuneeee.js

--- kimuneeee.js
+++ kimuneeee.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   let index = 0;
   const visibleText = document.getElementById("visible-text");
 
+  // フォントサイズを小さくする
+  visibleText.style.fontSize = "12px"; // 小さなフォントサイズに変更
+
   const intervalId = setInterval(() => {
     if (index < fullText.length) {
       visibleText.textContent += fullText[index];
